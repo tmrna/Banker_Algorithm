14f3416f6b145610b48790ca4f53c5d2a71982e3
RD <r.deans@outlook.com>
2022-11-14 18:26:07 -0500

    Added:
    - allocator (class)
    - matrix (class)
    - row (class)
    - reader (namespace)
    - writer (namespace)
    - rowCtor (test)

---
+++ Banker_Algo/lib/row.hpp
@@ -0,0 +1,39 @@
+#ifndef ROW_HPP
+#define ROW_HPP
+
+#include <vector>
+#include <iostream>
+
+class row{
+
+private:
+
+std::vector<int> values;
+
+public:
+
+// Default Ctor
+row();
+// Ctor (from vec)
+row(const std::vector<unsigned>&);
+// Cpy ctor
+row(const row&);
+// Dtor, handled by stl
+~row(){}
+
+// prints current row, matrix will use std::endl to clear buffer.
+void print(){for(unsigned i = 0; i < values.size(); i++) std::cout << values[i]; std::cout << "\n";}
+
+bool operator<(const row&);
+bool operator>(const row&);
+bool operator==(const row&);
+bool operator<=(const row& rhs){return *this < rhs && *this == rhs;}
+bool operator>=(const row& rhs){return *this > rhs && *this == rhs;}
+
+row operator-(const row&);
+row operator+(const row&);
+row& operator+=(const row& rhs){*this = *this + rhs; return *this;}
+row& operator-=(const row& rhs){*this = *this - rhs; return *this;}
+};
+
+#endif
