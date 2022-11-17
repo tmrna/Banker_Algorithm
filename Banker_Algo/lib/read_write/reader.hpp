dbba0aa20f605907bf0b02890cd097fd7968f1f8
RD <r.deans@outlook.com>
2022-11-17 18:41:18 -0500

    drafting:
    - reader.hpp

---
+++ Banker_Algo/lib/read_write/reader.hpp
@@ -0,0 +1,28 @@
+#ifndef READER_HPP
+#define READER_HPP
+
+#include "generator.hpp"
+
+class reader{
+
+private:
+    generator gen;
+public:
+
+    reader(){}
+    ~reader(){}
+
+    reader(const char*);
+    reader(std::string);
+    reader(generator);
+
+    std::vector<std::vector<int>> getAllocated();
+
+    std::vector<std::vector<int>> getMaximums();
+
+    std::vector<int> getAvail();
+
+
+};
+
+#endif
