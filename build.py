6be33c2b4d78a61455b63f2a2f09dc7558fbb8b7
RD <r.deans@outlook.com>
2022-11-20 10:41:23 -0500

    build script

--- build.py
+++ build.py
@@ -1,4 +1,20 @@
 import os
 
+starting_path = os.getcwd()
+bnkr_alg = starting_path + "/Banker_Algo"
+build_path = bnkr_alg + "/build"
+
+os.chdir(bnkr_alg)
+
 if(not os.path.exists("build")):
     os.system("mkdir build")
+
+os.chdir(build_path)
+
+os.system("cmake ..")
+
+os.system("make")
+
+os.system("mv Banker " + starting_path)
+
+os.chdir(starting_path)
